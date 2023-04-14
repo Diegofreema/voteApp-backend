982999852b9261340885975dd29f9abdde9f9009
Diego <diegofreeman78@gmail.com>
2023-04-14 23:17:40 +0100

    rime

--- models/poll.js
+++ models/poll.js
@@ -8,24 +8,29 @@ const optionSChema = new mongoose.Schema({
   },
 });
 
-const pollSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+const pollSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    question: String,
+    options: [optionSChema],
+    voted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    created: {
+      type: Date,
+      default: Date.now(),
+    },
+    expiresIn: {
+      type: Date,
+      default: new Date(Date.now() + 1000 * 86400),
+    },
   },
-  question: String,
-  options: [optionSChema],
-  voted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  created: {
-    type: Date,
-    default: Date.now(),
-  },
-  expiresIn: {
-    type: Date,
-    default: new Date(Date.now() + 1000 * 86400),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Poll = mongoose.model('Poll', pollSchema);
 
-module.exports = Poll
+module.exports = Poll;
