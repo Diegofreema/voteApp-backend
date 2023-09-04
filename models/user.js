259d9e6ddaff6d3476e86dbc5f995cfd5e95bfa1
Diego <diegofreeman78@gmail.com>
2023-09-05 00:59:59 +0100

    dfgf

--- models/user.js
+++ models/user.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
     type: 'String',
     required: true,
     unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    trim: true,
   },
   password: {
     type: 'String',
